617e558a20387d183ec3d74863cd3f71f8974a1d
Tdneubeck <tdnkvc@umsystem.edu>
2023-03-06 20:44:45 -0600

    this is the effcient one, i took out the prints and moved it to the end of the loop

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,12 +2,10 @@ numlist = list(range(1,101))
 for x in numlist:
     if x%3==0 and x%5==0:
         x = "FizzBuzz"
-        print (x)
     elif x%3==0:
         x = "Fizz"
-        print (x)
     elif x%5==0:
         x = "Buzz"
-        print (x)
     else:
-        print(x)
+        x=x
+    print (x)
